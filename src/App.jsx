97865387b689a5aaa48e41ff7ef464fd9cd7eb9d
Anastasia <maly1204n@gmail.com>
2025-04-17 17:29:30 +0300

    feat: Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.sass'
+import TheNewsBlock from './components/TheNewsBlock'
 
 function App() {
 
   return (
     <>
-      
+      <div className='news-background'>
+        <div className='news-container'>
+          <TheNewsBlock></TheNewsBlock>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/TheNewsBlock.jsx
@@ -0,0 +1,11 @@
+import './TheNewsBlock.sass'
+
+export default function TheNewsBlock() {
+    return (
+        <>
+            <div className='news-block'>
+                <p>тест</p>
+            </div>
+        </>
+    )
+}
